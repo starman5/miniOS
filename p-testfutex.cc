1189284aa80cb04e36581183c067f42debf59141
Matt Kiley <mdkiley@college.harvard.edu>
2022-05-08 21:24:27 -0400

    thing

--- p-testfutex.cc
+++ p-testfutex.cc
@@ -20,7 +20,8 @@ int function1(void* placeholder) {
 
 int function2(void* placeholder) {
     sys_msleep(100);
- 
+
+    *x = 3;
 }
  
 int test1() {
@@ -28,7 +29,7 @@ int test1() {
     char* stack1 = reinterpret_cast<char*>(
         round_up(reinterpret_cast<uintptr_t>(end), PAGESIZE) + 16 * PAGESIZE
     );
-    sys_clone(&function1, nullptr, stack1);
+    sys_clone(function1, nullptr, stack1);
  
     // block current thread until the second thread wakes it up
     sys_futex(&futex, 101, 5);
@@ -37,7 +38,12 @@ int test1() {
 }
 
 int test2() {
-
+    int (*func2)(void*) = function2;
+    char* stack2 = reinterpret_cast<char*>(
+        round_up(reinterpret_cast<uintptr_t>(end), PAGESIZE) + 16 * PAGESIZE
+    );
+    sys_clone(function2, nullptr, stack2);
+    sys_badfutex(x, FUTEX_WAIT, 3);
 }
 
 void process_main() {
